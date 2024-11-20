4c2c08976d705285d33e7fdc15727b56f9b4f882
Anderson <62722855+anderdot@users.noreply.github.com>
2024-11-19 23:06:24 -0300

    feat: day two

--- server.js
+++ server.js
@@ -1,10 +1,40 @@
 import express from "express";
 
+const posts = [
+    {
+      id: 1,
+      description: "A cat smiling",
+      image: "http://placecats.com/millie/300/150"
+    },
+    {
+      id: 2,
+      description: "A sad cat",
+      image: "http://placecats.com/millie/300/150"
+    },
+    {
+      id: 3,
+      description: "A cat with a hat",
+      image: "http://placecats.com/millie/300/150"
+    },
+];
+
 const app = express();
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
 
-app.get("/", (req, res) => {
-  res.status(200).send("Hello World!");
+app.get("/posts", (req, res) => {
+  res.status(200).json(posts);
+});
+
+app.get("/posts/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const post = posts.find((post) => post.id === id);
+    if (post) {
+        res.status(200).json(post);
+    } else {
+        res.status(404).json({ message: "Post not found" });
+    }
 });
